931ae07b58fbea7d0aadcc20a3f5e8d1e0fe9c5a
Kicirtap <Kicirtap@users.noreply.github.com>
2022-11-15 20:52:59 -0300

    criacao services quizz

--- src/services/ApiURI.js
+++ src/services/ApiURI.js
@@ -2,10 +2,13 @@
 const BASE_URL = "http://localhost:8081/";
 const CONTOS_EP = "contos";
 const CONTO_EP = "conto";
-
+const QUIZZ_EP = "quizz";
+const QUIZZES_EP = "quizzes"
 
 export {
     BASE_URL,
     CONTOS_EP,
-    CONTO_EP
+    CONTO_EP,
+    QUIZZ_EP,
+    QUIZZES_EP
 }
