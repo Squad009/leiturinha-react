b122ec4994fcd37b53c91af07290383e910aac6d
Onirvanico <48625780+Onirvanico@users.noreply.github.com>
2022-11-16 14:09:49 -0300

    Adicionando navegação principal no perfil do aluno

--- src/components/perfil/PerfilAluno.jsx
+++ src/components/perfil/PerfilAluno.jsx
@@ -2,11 +2,12 @@ import InfoAluno from "./InfoAluno";
 import HeaderApp from "../../shared/HeaderApp";
 import UltimasLeituras from "./UltimasLeituras";
 import Footer from "../../shared/footer/Footer"
+import NavigationMain from '../../shared/header/NavigationMain';
 
 export default function PerfilAluno() {
     return(
         <>
-            <HeaderApp />
+            <HeaderApp navigationMain={<NavigationMain />} />
             <InfoAluno />
             <UltimasLeituras />
             <Footer />
